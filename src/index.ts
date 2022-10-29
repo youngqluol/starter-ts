a51d9daa5b1db4e90bd13d9476614c6338e59ab9
lyq <953342912@qq.com>
2022-10-29 14:11:57 +0800

    feat: add tsx

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-export const one = 1
-export const two = 2
+import Koa from 'koa'
+import Router from 'koa-router'
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', (ctx) => {
+  ctx.body = 'hello koa'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(1000, () => {
+  // eslint-disable-next-line no-console
+  console.log('running')
+})
