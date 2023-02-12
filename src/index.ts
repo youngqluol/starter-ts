f4c8dc0560f6d89302fa2fc01196da513051318a
lyq <953342912@qq.com>
2023-02-12 12:31:23 +0800

    update files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import Koa from 'koa'
-import Router from 'koa-router'
+export function main(str: string) {
+  console.log(str)
+}
 
-const app = new Koa()
-const router = new Router()
-
-router.get('/', (ctx) => {
-  ctx.body = 'hello koa'
-})
-
-app.use(router.routes()).use(router.allowedMethods())
-
-app.listen(1000, () => {
-  // eslint-disable-next-line no-console
-  console.log('running')
-})
